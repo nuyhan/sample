a87b5a2d531dc914769f4e50e0a28e434b5bc1e0
nahyun <kimna0317@naver.com>
2023-07-14 23:17:10 +0900

    계정 별로 데이터 저장 가능

--- app/src/main/java/com/example/sample/SubActivity.java
+++ app/src/main/java/com/example/sample/SubActivity.java
@@ -26,7 +26,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 
-
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseError;
@@ -68,30 +69,6 @@ public class SubActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
         listView.setAdapter(adapter);
 
-        // ValueEventListener for data persistence
-        dataListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                data.clear(); // Clear previous data
-                for (DataSnapshot dataSnapshotItem : dataSnapshot.getChildren()) {
-                    ProductItem item = dataSnapshotItem.getValue(ProductItem.class);
-                    if (item != null) {
-                        String itemName = item.getName();
-                        String expiryDate = item.getExpiryDate();
-                        int quantity = item.getQuantity();
-
-                        addItem(itemName, expiryDate, quantity);
-                    }
-                }
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Toast.makeText(SubActivity.this, "Failed to load data", Toast.LENGTH_SHORT).show();
-            }
-        };
-        mDatabase.addValueEventListener(dataListener);
 
         // 데이터 로드 및 표시
         loadAndDisplayData();
@@ -137,12 +114,130 @@ public class SubActivity extends AppCompatActivity {
     }
 
     private void loadAndDisplayData() {
-        // Implement your data loading logic here
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String userUid = currentUser.getUid();
+            String userPath = "users/" + userUid + "/productItems";
+
+            DatabaseReference userRef = mDatabase.child(userPath);
+            userRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    data.clear(); // Clear previous data
+                    for (DataSnapshot dataSnapshotItem : dataSnapshot.getChildren()) {
+                        ProductItem item = dataSnapshotItem.getValue(ProductItem.class);
+                        if (item != null) {
+                            String itemName = item.getName();
+                            String expiryDate = item.getExpiryDate();
+                            int quantity = item.getQuantity();
+
+                            addItem(itemName, expiryDate, quantity);
+                        }
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Toast.makeText(SubActivity.this, "Failed to load data", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+
+    private void saveToFirebase(ProductItem productItem) {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String userUid = currentUser.getUid();
+            String userPath = "users/" + userUid + "/productItems"; // 사용자별 경로 생성
+
+            DatabaseReference userRef = mDatabase.child(userPath);
+            String key = userRef.push().getKey();
+            if (key != null) {
+                userRef.child(key).setValue(productItem)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(SubActivity.this, "상품이 추가되었습니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(SubActivity.this, "상품 추가에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        }
     }
 
     private void showAddItemDialog() {
-        // Implement your dialog logic here
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_add_item, null);
+        builder.setView(dialogView)
+                .setTitle("상품 추가")
+                .setPositiveButton("추가", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        EditText editItemName = dialogView.findViewById(R.id.edit_item_name);
+                        String itemName = editItemName.getText().toString();
+
+                        DatePicker datePicker = dialogView.findViewById(R.id.datepicker_expiry);
+                        int year = datePicker.getYear();
+                        int month = datePicker.getMonth();
+                        int day = datePicker.getDayOfMonth();
+                        String expiryDate = String.format("%04d-%02d-%02d", year, month + 1, day); // 유통 기한을 YYYY-MM-DD 형식으로 변환하여 저장
+
+                        TextView textQuantity = dialogView.findViewById(R.id.text_quantity);
+                        int quantity = Integer.parseInt(textQuantity.getText().toString()); // 수량을 가져옴
+
+
+                        // Firebase Realtime Database에 생성된 ProductItem을 저장합니다.
+                        saveToFirebase(new ProductItem(itemName, expiryDate, quantity));
+
+
+                        addItem(itemName, expiryDate, quantity); // 아이템 추가 메서드 호출
+                    }
+                })
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        // 수량 증가 버튼 클릭 이벤트 처리
+        Button buttonIncrease = dialogView.findViewById(R.id.button_increase);
+        buttonIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textQuantity = dialogView.findViewById(R.id.text_quantity);
+                int quantity = Integer.parseInt(textQuantity.getText().toString());
+                quantity++;
+                textQuantity.setText(String.valueOf(quantity));
+            }
+        });
+
+        // 수량 감소 버튼 클릭 이벤트 처리
+        Button buttonDecrease = dialogView.findViewById(R.id.button_decrease);
+        buttonDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textQuantity = dialogView.findViewById(R.id.text_quantity);
+                int quantity = Integer.parseInt(textQuantity.getText().toString());
+                if (quantity > 1) {
+                    quantity--;
+                    textQuantity.setText(String.valueOf(quantity));
+                }
+            }
+        });
     }
+
 
     private void addItem(String newItem, String expiryDate, int quantity) {
         String itemDetails = newItem + " - 유통 기한: " + expiryDate + ", 수량: " + quantity;
